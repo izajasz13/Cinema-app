ec2accf234d5d7ea6325f96d4ca059711b9a3d03
izajasz13 <karoldd13@gmail.com>
2019-10-04 19:24:27 +0200

    Display info of selected movie

--- src/components/List.js
+++ src/components/List.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Movie from './Movie';
 
 class List extends React.Component{
+    selectMovie = (e) =>{
+        e.preventDefault();
+
+        const index = e.currentTarget.dataset.index;
+        this.props.selectMovie(index)
+    }
+
     render(){
         return(
             <div className="ui grid">
-                {this.props.movies.map(ele => 
-                    <div className = "four wide column one column stackable center aligned page grid" key={ele.title}>
+                {this.props.movies.map((ele, index) => 
+                    <div className = "four wide column one column stackable center aligned page grid" onClick={this.selectMovie} key={ele.title} data-index={index}>
                         <Movie movie = {ele} />
                     </div>
                 )}
